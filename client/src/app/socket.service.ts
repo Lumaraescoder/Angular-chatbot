1c5f03cafbebf5cd58892c49b9eeff00f939a268
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2022-08-29 12:36:43 -0300

    up

---
+++ client/src/app/socket.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import * as io from 'socket.io-client';
+
+
+@Injectable()
+export class SocketService {
+
+ private socket = io('http://localhost:8000');
+ constructor() { }
+
+ sendMessage(data: any) {
+  this.socket.emit('message', data);
+ }
+
+ receivedReply() {
+  const observable = new Observable<any>(observer => {
+   this.socket.on('reply', (data: any) => {
+    observer.next(data);
+   });
+   return () => {
+    this.socket.disconnect();
+   };
+  });
+  return observable;
+ }
+
+}
